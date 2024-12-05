b55380c4fdc9a0b4fe99f3e9343aeab70e527fa4
Ujjawal Singh <ujjawal95@gmail.com>
2024-12-06 04:16:59 +0530

    Added: Day 05 - Challange

---
+++ 05/hint.md
@@ -0,0 +1,2 @@
+Try to rearrange the characters in the two strings in such a way
+that you're able to compare the strings using the equality operator.
---
+++ 05/index.js
@@ -0,0 +1,31 @@
+/* 
+This Christmas, you’ve been tasked with running an anagram quiz at 
+the family gathering.
+
+You have been given a list of anagrams, but you suspect that some 
+of the anagram pairs might be incorrect.
+
+Your job is to write a JavaScript function to loop through the array
+and filter out any pairs that aren’t actually anagrams.
+
+For this challenge, spaces will be ignored, so "Be The Helm" would 
+be considered a valid anagram of "Bethlehem".
+*/ 
+
+let anagrams = [
+    ["Can Assault", "Santa Claus"],
+    ["Refreshed Erudite Londoner", "Rudolf the Red Nose Reindeer"],
+    ["Frosty The Snowman", "Honesty Warms Front"],
+    ["Drastic Charms", "Christmas Cards"],
+    ["Congress Liar", "Carol Singers"],
+    ["The Tin Glints", "Silent Night"],
+    ["Be The Helm", "Betlehem"],
+    ["Is Car Thieves", "Christmas Eve"]
+];
+
+function findAnagrams(array){
+    // write your code here
+}
+
+
+ 
