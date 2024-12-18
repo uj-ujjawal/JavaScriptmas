d9c016c207316b9e0cf6e2567c76a3670facff3e
Ujjawal Singh <ujjawal95@gmail.com>
2024-12-18 23:23:22 +0530

    Day 17: challenge Solved✅

--- 17/index.js
+++ 17/index.js
@@ -2,5 +2,5 @@ const form = document.getElementsByClassName("signup-form")[0];
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  document.getElementsByClassName("message")[0].style.display = "inline";
+  document.getElementsByClassName("message")[0].style.display = "block";
 });
