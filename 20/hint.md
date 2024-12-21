3fa7656ca25b5ea99ce0e50d5b9da97817e07ac8
Ujjawal Singh <ujjawal95@gmail.com>
2024-12-21 06:10:37 +0530

    Added: Day 20 - Challange

---
+++ 20/hint.md
@@ -0,0 +1,5 @@
+### HINTS
+
+- Use an array to store previously generated names.
+- Use the Array.find() method to match initials.
+- Have a "render" function that updates the UI every time a new name is generated
---
+++ 20/index.js
@@ -0,0 +1,71 @@
+const elfFirstNames = [
+  "Aurora",
+  "Blitzen",
+  "Crispin",
+  "Dazzle",
+  "Evergreen",
+  "Frost",
+  "Glimmer",
+  "Holly",
+  "Icicle",
+  "Joyful",
+  "Kringle",
+  "Luna",
+  "Merry",
+  "Nutmeg",
+  "Olwen",
+  "Pine",
+  "Quill",
+  "Razzle",
+  "Sparkle",
+  "Tinsel",
+  "Umbra",
+  "Vixen",
+  "Whisk",
+  "Xylo",
+  "Yule",
+  "Zippy",
+];
+
+const elfLastNames = [
+  "Applecheeks",
+  "Bells",
+  "Candycane",
+  "Dazzlebright",
+  "Everbright",
+  "Frostwhisk",
+  "Gingersnap",
+  "Hollyberry",
+  "Icestorm",
+  "Jovial",
+  "Kindleflame",
+  "Lightwhisper",
+  "Merrysprout",
+  "Nutcracker",
+  "Oakenleaf",
+  "Peppermint",
+  "Quicksilver",
+  "Raindrop",
+  "Snowdust",
+  "Twinkletoes",
+  "Underwood",
+  "Velvet",
+  "Winterberry",
+  "Xylospark",
+  "Yuletide",
+  "Zestwind",
+];
+
+/*
+ * 🎅 Task:
+ * - Generate an elf first and last name that matches the user’s first and last name initials, then display it on the screen.
+ * - Example: if the user’s name is "John Doe," the elf name could be "Joyful Dazzle."
+ * - Display the generated elf names in the "Registered Employees" list.
+ */
+
+/*
+ * 🌟 Stretch Goals:
+ * - Generate the elf names using an LLM API (like HuggingFace).
+ * - Don't save the same name twice. (not necessary for the normal task)
+ * - Make sure to use Scrimba's environment variables feature so you don't expose your API key
+ */
