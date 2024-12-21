c524108beb1cf6807a311c497ee67dbedfc1bc71
Ujjawal Singh <ujjawal95@gmail.com>
2024-12-21 06:11:50 +0530

    Added: Day 21 - Challange

---
+++ 21/hint.md
@@ -0,0 +1,9 @@
+As ever, there are many ways of solving this challenge. Here are some pointers for one way to do it.
+
+1. Create an empty object to track the total number of requests for each toy.
+2. Loop through "locations" and loop through "toys" for that location.
+3. Object.entries could be useful for extracting the information you need.
+4. Make sure no toy is repeated in the object you created in step 1.
+5. Iterate through that object and track the highest number.
+
+You could also complete this challenge using the .reduce() method.
---
+++ 21/index.js
@@ -0,0 +1,16 @@
+import { toysRequested } from "./data.js";
+
+/*
+The run up to Christmas is quite a data-intensive time for Santa. In order to understand market trends, Santa's Data Elves have collated sample children’s wish list data from 4 locations and now need to know which was the most popular toy requested overall. This will help with procurement for next year.
+
+**Task**
+- Your task is to find the most requested toy from the array of objects “toysRequested”. But remember: some toys appear in more than one location!
+
+Expected output: "The most popular toy is 🎲 board games with 9000 requests.""
+
+**Stretch Goal**
+- Complete this challenge using the .flatMap() method to work with the various "toys" arrays.
+
+*/
+
+console.log(`The most popular toy is <TOY> with <NUMBER> requests.`);
